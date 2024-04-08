de2fd17edd36b8fda524f37085dcabe47be8bfca
Juan Fernandez <2109061@upy.edu.mx>
2024-04-08 01:46:02 -0600

    finished

--- HPC/CA3/CA3_5_CC/setup.py
+++ HPC/CA3/CA3_5_CC/setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules =cythonize(["cython1.pyx", "cython2.pyx", "cython3.pyx", "cython4.pyx"]))
+setup(ext_modules =cythonize("cython2_1.pyx"))

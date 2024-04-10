98614aded82a5f2507a53ee04360e29963d93afa
Juan Fernandez <2109061@upy.edu.mx>
2024-04-09 18:01:44 -0600

    clas abr 9 2024

--- HPC/other/hpc_shared_a.py
+++ HPC/other/hpc_shared_a.py
@@ -1,8 +1,28 @@
 import multiprocessing
 
 class Process(multiprocessing.Process):
-    def __init__(self):
+    def __init__(self,counter, lock):
         super(Process, self).__init__()
+        self.counter = counter
+        self.lock = lock
+
+
+
+    def run(self):
+        for i in range(1_000):
+            with self.lock:
+                self.counter.value += 1
 
     
+def main():
+    counter = multiprocessing.Value('i', lock=True)
+    lock = multiprocessing.Lock()
+    process = [Process(counter,lock) for i in range(4)]
+
+    [p.start() for p in process]
+    [p.join() for p in process]
     
+    print(counter.value)
+
+if __name__ == '__main__':
+    main()
